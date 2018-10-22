65fb3afad8ea088c56b066b23dd537c7d8458960
Boris <bd@whitescape.com>
2018-10-22 10:42:39 +0300

    Babel app build

--- webpack.config.js
+++ webpack.config.js
@@ -12,4 +12,26 @@ module.exports = {
     path: path.resolve(__dirname, 'app', 'build'),
     filename: 'index.js',
   },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: 'babel-loader',
+            options: {
+              presets: [
+                ['@babel/preset-env', {
+                  targets: {
+                    browsers: ['>0.25%', 'not ie 11', 'not op_mini all']
+                  }
+                }]
+              ]
+            }
+          }
+        ],
+      }
+    ]
+  }
 }
